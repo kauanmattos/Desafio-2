79afc423e934ba2bada01afa554875212e84ef78
Kauan Mattos <kauanosmattos@gmail.com>
2025-06-29 20:21:23 -0300

    Programa simples finalizado

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// document.getElementById('result') = buscamos o elemento pela perm_identity
+
+// .innerHTML = propriedade usada para obter ou definir o conteúdo HTML de um elemento
+
+// 'Approved' = nomeamos como aprovado 
+
 let productValue = Number (prompt('Enter the product value'))
 
 if (productValue >= 20) {
